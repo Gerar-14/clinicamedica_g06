1396aca917590296110ef8a6eafbd2f8b0f52a43
anth-14 <mh16022@clases.edu.sv>
2022-05-27 19:19:03 -0600

    self routes comentado en application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module ClinicaMedicaG06
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.i18n.default_locale = 'es'
-    config.exceptions_app = self.routes
+    #config.exceptions_app = self.routes
 
     # Configuration for the application, engines, and railties goes here.
     #
