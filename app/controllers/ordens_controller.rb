f11fc28993e343f5813daefd92e43c77efae68ed
GerardoPeralta21 <47636271+GerardoPeralta21@users.noreply.github.com>
2022-05-30 23:01:13 -0600

    Se agrego seguridad, ademas en la pantalla ordenes se dividio la pantalla segun el rol activo, laboratorista, jefe de laboratorio y recepcionista

--- app/controllers/ordens_controller.rb
+++ app/controllers/ordens_controller.rb
@@ -1,16 +1,40 @@
-class OrdensController < ApplicationController
+class OrdensController < HomeController
   before_action :set_orden, only: %i[ show edit update destroy seleccionado finalizado]
 
   # GET /ordens or /ordens.json
   def index
+    #Encontraremos el rol del usuario activo
+    @usuario = current_user.id
+    @empleado = obteniendo_empleado(@usuario)
+    @id_rol = rol_empleado(@empleado.id)
+
+    #id_rol = 3 es laboratorista
+    #id_rol = 4 es jefe de laboratorista
+    #id_rol = 2 recepcionista
+
     #@ordens = Orden.all
     @id_usuario_actual = current_user.id
-    #Esta consulta SQL hace lo mismo que la linea 11
-    #@ordens = Orden.find_by_sql(["SELECT ordens.id, ordens.fecha_examen, ordens.paciente_id, ordens.laboratory_worker_id FROM ordens INNER JOIN laboratory_workers on ordens.laboratory_worker_id = laboratory_workers.id INNER JOIN empleados on laboratory_workers.empleado_id = empleados.id INNER JOIN users on empleados.user_id = users.id WHERE empleados.profesion = 'Laboratorista' and empleados.user_id = ? ", @id_usuario_actual])
-    #Esta consulta SQL es con los metodos de ActiveRecord que usa rails
-    @ordens = Orden.select(:id,:fecha_examen,:paciente_id,:laboratory_worker_id, :estado)
-    .joins(laboratory_worker: [empleado: :user])
-    .where('empleados.profesion' => 'Laboratorista').where('empleados.user_id' => @id_usuario_actual)
+
+    if @id_rol[0] == 3 #Laboratorista
+      #Esta consulta SQL hace lo mismo que la linea 11
+      #@ordens = Orden.find_by_sql(["SELECT ordens.id, ordens.fecha_examen, ordens.paciente_id, ordens.laboratory_worker_id FROM ordens INNER JOIN laboratory_workers on ordens.laboratory_worker_id = laboratory_workers.id INNER JOIN empleados on laboratory_workers.empleado_id = empleados.id INNER JOIN users on empleados.user_id = users.id WHERE empleados.profesion = 'Laboratorista' and empleados.user_id = ? ", @id_usuario_actual])
+      #Esta consulta SQL es con los metodos de ActiveRecord que usa rails
+      @ordens = Orden.select(:id,:fecha_examen,:paciente_id,:laboratory_worker_id, :estado)
+      .joins(laboratory_worker: [empleado: :user])
+      .where('empleados.profesion' => 'Laboratorista').where('empleados.user_id' => @id_usuario_actual)
+
+    elsif @id_rol[0] == 2 #Recepcionista
+      @ordens = get_orders_recepcionista()
+
+    elsif @id_rol[0] == 4 #Jefe de laboratorio
+      @ordens = Orden.all
+    end
+
+  end
+
+  def get_orders_recepcionista()
+    @orders_jefes = Orden.find_by_sql(["SELECT * from ordens where estado = 1"])
+    return @orders_jefes
   end
 
   # GET /ordens/1 or /ordens/1.json
