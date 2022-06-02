75c4dff205c230b96d153a34b8a8c91e6bbd3e96
anth-14 <mh16022@clases.edu.sv>
2022-06-02 01:27:03 -0600

    solo insert v4 valores referencias

--- db/migrate/20220522190819_create_value_references.rb
+++ db/migrate/20220522190819_create_value_references.rb
@@ -1,7 +1,7 @@
 class CreateValueReferences < ActiveRecord::Migration[7.0]
   def change
     create_table :value_references do |t|
-      t.string :value_reference, limit: 30, null: false
+      t.string :value_reference, limit: 100, null: false
       t.references :parametro, null: false, foreign_key: true
 
       t.timestamps
