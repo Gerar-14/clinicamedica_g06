4cdb7409e9f3e6dd5c2392273ae63bb98c74cc06
detad <danielenrique9919@hotmail.com>
2022-06-01 18:16:38 -0600

    Consulta de municipio y departamentos

--- app/controllers/reporte_controller.rb
+++ app/controllers/reporte_controller.rb
@@ -1,4 +1,35 @@
-class ReporteController < ApplicationController
+class ReporteController < HomeController
   def index
+    @reporteCantidadPacientesPorTipoSangre = cantidadPacientePorTipeoSangrePorZonaGeografica("2022-05-26", "2022-05-31", 10, 1)
+    @reporteCantidadPacientesPorTipoSangre.each do |e|
+      puts(e.nombre_departamento)
+      puts(e.nombre_municipio)
+      puts(e.nombre_tipo_sangre)
+      puts(e.cantidad)
+    end
+  end
+
+  def cantidadPacientePorTipeoSangrePorZonaGeografica(fechaInicio, fechaFin, idDepartamento, idMunicipio)
+    @cantidadPacientesPorTipoSangre = ""
+    @consultaSQL = "select d.nombre_departamento, m.nombre_municipio , p.tipo_sangre_id, ts.nombre_tipo_sangre, count(p.id) cantidad  from pacientes p
+      inner join tipo_sangres ts on ts.id = p.tipo_sangre_id
+      inner join municipios m on p.municipio_id = m.id
+      inner join departamentos d on m.departamento_id = d.id
+      inner join ordens o on o.paciente_id = p.id
+      where (nullif(str_to_date(o.created_at,'%Y-%m-%d'), from_days(0))  between str_to_date(?,  '%Y-%m-%d')
+      and str_to_date(?,  '%Y-%m-%d')) and d.id = ? "
+    if idMunicipio.blank?
+      #No posee municipio
+      @consultaSQL = @consultaSQL + "group by m.nombre_municipio
+      order by d.nombre_departamento , count(ts.id)"
+      @cantidadPacientesPorTipoSangre = Paciente.find_by_sql([@consultaSQL, fechaInicio, fechaFin, idDepartamento])
+    else
+      #Posee municipio
+      @consultaSQL = @consultaSQL + "and m.id = ? group by m.nombre_municipio
+      order by d.nombre_departamento , count(ts.id)"
+      @cantidadPacientesPorTipoSangre = Paciente.find_by_sql([@consultaSQL, fechaInicio, fechaFin, idDepartamento, idMunicipio])
+    end
+    return @cantidadPacientesPorTipoSangre
+    # @cantidadPacientesPorTipoSangre = Paciente.find_by_sql(["", fechaInicio, fechaFin])
   end
 end
